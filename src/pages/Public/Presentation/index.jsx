3aebf538a447cbce111b843afd2706b5077db506
jgtb <joao_gabriel200@hotmail.com>
2019-12-28 14:41:08 -0300

    att

--- src/pages/Public/Presentation/index.jsx
+++ src/pages/Public/Presentation/index.jsx
@@ -15,30 +15,30 @@ import {
 const Component = () => {
   const slidesStyles = { height: '100%', color: '#cbccce', backgroundColor: '#53565d', '--bullet-background-active': '#97999d' }
   const slideStyle = { display: 'block' }
-  const slideImageStyle = { height: '40%', marginTop: '35px' }
-  const slideButtonStyle = { '--color': '#ffffff', '--border-color': '#a9aaae', '--box-shadow': 'none', marginLeft: '25px', marginRight: '25px' }
-  const slideButtonLoginStyle = { ...slideButtonStyle, marginTop: '25px', marginBottom: '15px' }
+  const imageStyle = { height: '40%', marginTop: '35px' }
+  const buttonStyle = { '--color': '#ffffff', '--border-color': '#a9aaae', '--box-shadow': 'none', marginLeft: '25px', marginRight: '25px' }
+  const buttonLoginStyle = { ...buttonStyle, marginTop: '25px', marginBottom: '15px' }
 
   const WelcomeSlide = () => (
     <IonSlide style={slideStyle}>
-      <img style={slideImageStyle} src={ionicLogo} alt="Ionic" />
+      <img style={imageStyle} src={ionicLogo} alt="Ionic" />
       <h1>Ionic</h1>
     </IonSlide>
   )
 
   const HomeSlide = () => (
     <IonSlide style={slideStyle}>
-      <img style={slideImageStyle} src={reactLogo} alt="React" />
+      <img style={imageStyle} src={reactLogo} alt="React" />
       <h1>React</h1>
     </IonSlide>
   )
 
   const Slide = () => (
     <IonSlide style={slideStyle}>
-      <img style={slideImageStyle} src={ionicReactLogo} alt="Ionic" />
+      <img style={imageStyle} src={ionicReactLogo} alt="Ionic" />
       <h1>Ionic + React</h1>
-      <IonButton style={slideButtonLoginStyle} shape="round" expand="block" fill="outline" routerLink="/login">Login</IonButton>
-      <IonButton style={slideButtonStyle} shape="round" expand="block" fill="outline" routerLink="/register">Cadastro</IonButton>
+      <IonButton style={buttonLoginStyle} shape="round" expand="block" fill="outline" routerLink="/login">Login</IonButton>
+      <IonButton style={buttonStyle} shape="round" expand="block" fill="outline" routerLink="/register">Cadastro</IonButton>
     </IonSlide>
   )
 
